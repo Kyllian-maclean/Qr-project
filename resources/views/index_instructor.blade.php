c05023ed3e19be35e03da6f424e5984d2374ad8a
valuh02 <valeria2004828@gmail.com>
2023-09-06 18:23:12 -0500

    Cambios admin2

--- resources/views/index_instructor.blade.php
+++ resources/views/index_instructor.blade.php
@@ -8,12 +8,13 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11.0.19/dist/sweetalert2.min.css">
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.0.19/dist/sweetalert2.all.min.js"></script>
+    <link rel="stylesheet" href="{{url('assets/css/inst.css')}}">
 
     <title>Biometric</title>
 
 </head>
 <body>
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <nav class="navbar navbar-expand-sm bg-dark navbar-dark py-2">
     <div class="container">
       <a class="navbar-brand" href="#">Biometric</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
