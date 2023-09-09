873622c62a64a9f7b51b4365f3c8f8e005d0e70f
Kyllian Maclean Peña Trujillo <133929057+Kyllian-maclean@users.noreply.github.com>
2023-09-09 18:33:44 -0500

    arreglos basicos

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -11,12 +11,14 @@
                     <div class="input-box">
                         <form method="POST" action="{{ route('login') }}">
                             @csrf
-                            <header>Iniciar sesion</header>
+                            <header>
+                                <h1>Iniciar sesion</h1>
+                            </header>
                             
                             <div class="input-field">
 
                                 <div class="input field">
-                                    <select id="role" class="input @error('role') is-invalid @enderror" name="role" required>
+                                    <select id="role" class="form-select @error('role') is-invalid @enderror" name="role" required>
                                         <option value="" disabled selected>Selecciona un Rol</option>
                                         <option value="1">Administrador</option>
                                         <option value="2">Instructor</option>
@@ -46,7 +48,7 @@
 
                             <div class="input-field">
                                 <input id="password" type="password" class="input @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-                                <label for="password" class="input">{{ __('Contraseña') }}</label>
+                                <label for="password">{{ __('Contraseña') }}</label>
 
                                     @error('password')
                                         <span class="invalid-feedback" role="alert">
@@ -55,15 +57,19 @@
                                     @enderror
                             </div>
 
-                            <div >
-                            
-                                    <div class="form-check">
-                                        <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                        <label class="form-check-label" for="remember">
-                                            {{ __('Recordar') }}
-                                        </label>
-                                    </div>
+                            <div class="form-check">
+                            <input id="inpLock" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+                                <label class="btn-lock" for="inpLock">
+                                    <svg width="18" height="20" viewBox="0 0 36 40">
+                                        <path class="lockb" d="M27 27C27 34.1797 21.1797 40 14 40C6.8203 40 1 34.1797 1 27C1 19.8203 6.8203 14 14 14C21.1797 14 27 19.8203 27 27ZM15.6298 26.5191C16.4544 25.9845 17 25.056 17 24C17 22.3431 15.6569 21 14 21C12.3431 21 11 22.3431 11 24C11 25.056 11.5456 25.9845 12.3702 26.5191L11 32H17L15.6298 26.5191Z"></path>
+                                        <path class="lock" d="M6 21V10C6 5.58172 9.58172 2 14 2V2C18.4183 2 22 5.58172 22 10V21"></path>
+                                        <path class="bling" d="M29 20L31 22"></path>
+                                        <path class="bling" d="M31.5 15H34.5"></path>
+                                        <path class="bling" d="M29 10L31 8"></path>
+                                        
+                                    </svg>
+                                </label>
+                                <label for="">Recordarme</label>
                                 
                             </div>
 
